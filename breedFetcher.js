1251825db0e8b1393b673eec4a0e17d591d5d9fd
Rayesh <rayeshrajah@gmail.com>
2020-02-13 20:10:43 +0000

    test files added

--- breedFetcher.js
+++ breedFetcher.js
@@ -1,21 +1,19 @@
 const request = require("request");
-const arrayBreed = process.argv.slice(2);
 
-request(
-  `https://api.thecatapi.com/v1/breeds/search?q=${arrayBreed[0]}`,
-  (error, response, body) => {
-    if (!error) {
-      const data = JSON.parse(body);
-      console.log(data[0].description);
-      process.exit;
-    } else if (error === null) {
-      console.log(
-        "Sorry the breed is not found, or you may haave entered the breed name wrong. Please try again"
-      );
-      process.exit();
-    } else if (error !== null && response === undefined) {
-      console.log(`The domain name is the invalid and the error is ${error}`);
-      process.exit();
+const fetchBreedDescription = function(breedName, callback) {
+  request(
+    `https://api.thecatapi.com/v1/breeds/search?q=${breedName}`,
+    (error, response, body) => {
+      if (body === "[]") {
+        callback(error, body);
+      } else if (error) {
+        callback(error, null);
+      } else if (!error) {
+        const data = JSON.parse(body);
+        callback(null, data[0].description);
+      }
     }
-  }
-);
+  );
+};
+
+module.exports = { fetchBreedDescription };
